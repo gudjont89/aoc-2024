7ef02c5f67d24a93bbb6749063e2f2757f221720
Gudjon Teitur Sigurdarson <gudjon@dte.ai>
2024-12-10 16:30:30 +0000

    Day ten - refactoring

--- aoc-2024/src/dec_10.rs
+++ aoc-2024/src/dec_10.rs
@@ -5,28 +5,30 @@ use itertools::Itertools;
 use crate::util::{cardinal_directions, position_map_from_text_lines, read_from_file, Direction, Position};
 
 pub fn run_first(is_real: bool) -> usize {
-    let lines = read_from_file(is_real, 10, None);
-    let parse_from_char= |c: char| c.to_digit(10).unwrap() as usize;
-    let trail_map = position_map_from_text_lines(&lines, parse_from_char);
+    let trail_map = get_trail_map(is_real);
 
-    trail_map
-        .iter()
-        .filter(|(_, h)| **h == TRAILHEAD_HEIGHT)
-        .map(|(p, _)| Trailhead::new(&trail_map, *p) )
-        .map(|th| th.calculate_score())
-        .sum::<usize>()
+    calculate_from_trail_map(&trail_map, |trailhead| trailhead.calculate_score())
 }
 
 pub fn run_second(is_real: bool) -> usize {
+    let trail_map = get_trail_map(is_real);
+
+    calculate_from_trail_map(&trail_map, |trailhead| trailhead.calculate_rating())
+}
+
+fn get_trail_map(is_real: bool) -> HashMap<Position, usize> {
     let lines = read_from_file(is_real, 10, None);
     let parse_from_char= |c: char| c.to_digit(10).unwrap() as usize;
-    let trail_map = position_map_from_text_lines(&lines, parse_from_char);
 
+    position_map_from_text_lines(&lines, parse_from_char)
+}
+
+fn calculate_from_trail_map(trail_map: &HashMap<Position, usize>, calculator: fn(Trailhead) -> usize) -> usize {
     trail_map
         .iter()
         .filter(|(_, h)| **h == TRAILHEAD_HEIGHT)
         .map(|(p, _)| Trailhead::new(&trail_map, *p) )
-        .map(|th| th.calculate_rating())
+        .map(|th| calculator(th))
         .sum::<usize>()
 }
 
@@ -40,29 +42,28 @@ struct Trailhead {
 impl Trailhead {
     fn new(trail_map: &HashMap<Position, usize>, position: Position) -> Self {
         let children = Tile::populate_children(trail_map, &position, None);
-        let tile = Tile { position, height: 0, transition_type: TransitionType::GradualIncline(children) };
+        let tile = Tile::GradualIncline(children);
 
         Self { tile, }
     }
 
     fn calculate_score(&self) -> usize {
-        self.tile.transition_type.summit_positions()
+        self.tile.summit_positions()
             .iter()
             .unique()
             .count()
     }
 
     fn calculate_rating(&self) -> usize {
-        self.tile.transition_type.summit_positions()
+        self.tile.summit_positions()
             .iter()
             .count()
     }
 }
 
-struct Tile {
-    position: Position,
-    height: usize,
-    transition_type: TransitionType,
+enum Tile {
+    GradualIncline(Vec<Box<Tile>>),
+    Summit(Position),
 }
 
 impl Tile {
@@ -82,20 +83,20 @@ impl Tile {
         }
 
         if tile_height == SUMMIT_HEIGHT {
-            return Some(Self { position: tile_position, height: tile_height, transition_type: TransitionType::Summit(tile_position) })
+            return Some(Self::Summit(tile_position))
         }
 
         let banned_direction = &hiking_direction.reverse();
         let children = Tile::populate_children(trail_map, &tile_position, Some(banned_direction));
 
-        Some(Self { position: tile_position, height: tile_height, transition_type: TransitionType::GradualIncline(children) })
+        Some(Self::GradualIncline(children))
     }
     
     fn populate_children(
         trail_map: &HashMap<Position, usize>, 
         position: &Position,
         banned_direction: Option<&Direction>,
-    ) -> Vec<Box<Tile>> {
+    ) -> Vec<Box<Self>> {
         cardinal_directions()
             .into_iter()
             .filter(|d| {
@@ -108,21 +109,14 @@ impl Tile {
             .map(|t| Box::new(t))
             .collect()
     }
-}
 
-enum TransitionType {
-    GradualIncline(Vec<Box<Tile>>),
-    Summit(Position),
-}
-
-impl TransitionType {
     fn summit_positions(&self) -> Vec<Position> {
         match self {
-            TransitionType::Summit(position) => vec![*position],
-            TransitionType::GradualIncline(child_tiles) => {
+            Tile::Summit(position) => vec![*position],
+            Tile::GradualIncline(child_tiles) => {
                 child_tiles
                     .iter()
-                    .flat_map(|ct| ct.transition_type.summit_positions())
+                    .flat_map(|ct| ct.summit_positions())
                     .collect::<Vec<Position>>()
             },
         }
