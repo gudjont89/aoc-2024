f3769e8defce9cd67a0c91bf13b61258459a24bd
Gudjon Teitur Sigurdarson <gudjon@dte.ai>
2024-12-11 23:22:11 +0000

    Day eleven - part two

--- aoc-2024/src/dec_11.rs
+++ aoc-2024/src/dec_11.rs
@@ -1,3 +1,7 @@
+use std::{collections::HashMap, time::Instant, vec};
+
+use itertools::Itertools;
+
 use crate::util::read_single_string_from_file;
 
 const STONE_MULTIPLIER: usize = 2024;
@@ -6,21 +10,10 @@ pub fn run_first(is_real: bool) -> usize {
     const NUMBER_OF_BLINKS: usize = 25;
 
     let line = read_single_string_from_file(is_real, 11, None);
+    let initial_stones = get_numbers_in_string(&line);
 
     convert_stones(
-        get_numbers_in_string(&line), 
-        NUMBER_OF_BLINKS)
-    .len()
-}
-
-pub fn run_second(is_real: bool) -> usize {
-    const NUMBER_OF_BLINKS: usize = 75;
-    // todo: this is too large a number, we need to rethink this. maybe a tree would be a good idea
-
-    let line = read_single_string_from_file(is_real, 11, None);
-
-    convert_stones(
-        get_numbers_in_string(&line), 
+        initial_stones,
         NUMBER_OF_BLINKS)
     .len()
 }
@@ -34,6 +27,80 @@ fn convert_stones(mut stones: Vec<usize>, number_of_blinks: usize) -> Vec<usize>
     }
 
     stones
+}
+
+pub fn run_second(is_real: bool) -> usize {
+    const NUMBER_OF_BLINKS: usize = 75;
+    
+    let line = read_single_string_from_file(is_real, 11, None);
+    let initial_stones = get_numbers_in_string(&line);
+    
+    let lookup_table_keys = get_lookup_table_keys(&initial_stones);
+
+    let mut lookup_table: HashMap<usize, HashMap<usize, usize>> = HashMap::new();
+
+    for i in 1..NUMBER_OF_BLINKS {
+        let value_to_score_table = populate_lookup_table(
+            lookup_table_keys.clone(), 
+            i,
+            &lookup_table
+        );
+        lookup_table.insert(i, value_to_score_table);
+    }
+
+    count_stones_after_blinking_collection(initial_stones, NUMBER_OF_BLINKS, &lookup_table)
+}
+
+fn get_lookup_table_keys(initial_stones: &[usize]) -> Vec<usize> {
+    let initial_stones = initial_stones
+        .iter()
+        .map(|x| *x)
+        .collect::<Vec<usize>>();
+
+    let converted_stones = convert_stones(initial_stones, 25);
+    let unique_valued_stones = converted_stones
+        .iter()
+        .unique()
+        .map(|&s| s)
+        .collect::<Vec<usize>>();
+    
+    unique_valued_stones
+        .iter()
+        .map(|&sv| (sv, converted_stones.iter().filter(|&&csv| csv == sv).count()))
+        .sorted_by(|a, b| b.1.cmp(&a.1))
+        .filter(|&(_, f)| f > 100)
+        .map(|(v, _)| v)
+        .collect::<Vec<usize>>()
+}
+
+fn populate_lookup_table(
+    stone_values: Vec<usize>, 
+    depth: usize,
+    lookup_table: &HashMap<usize, HashMap<usize, usize>>,
+) -> HashMap<usize, usize> {
+    stone_values
+        .iter()
+        .map(|&sv| (sv, count_stones_after_blinking(sv, depth, lookup_table)))
+        .collect()
+}
+
+fn count_stones_after_blinking_collection(
+    stones: Vec<usize>, 
+    number_of_blinks: usize,
+    lookup_table: &HashMap<usize, HashMap<usize, usize>>
+) -> usize {
+    stones
+        .iter()
+        .map(|s| count_stones_after_blinking(*s, number_of_blinks, lookup_table))
+        .sum()
+}
+
+fn count_stones_after_blinking(
+    stone: usize, 
+    number_of_blinks: usize,
+    lookup_table: &HashMap<usize, HashMap<usize, usize>>
+) -> usize {
+    StoneNode::initial(stone).count_children_recursively(number_of_blinks, lookup_table)
 }
 
 fn get_numbers_in_string(number_str: &str) -> Vec<usize> {
@@ -73,6 +140,47 @@ fn split_stone(stone: usize) -> Option<Vec<usize>> {
     Some(vec![msn, lsn])
 }
 
+struct StoneNode {
+    depth: usize,
+    value: usize,
+}
+
+impl StoneNode {
+    fn initial(value: usize) -> Self {
+        Self { depth: 0, value }
+    }
+
+    fn new(parent_depth: usize, value: usize) -> Self {
+        Self { depth: parent_depth + 1, value }
+    }
+
+    fn count_children_recursively(
+        &self, 
+        maximum_depth: usize,
+        lookup_table: &HashMap<usize, HashMap<usize, usize>>
+    ) -> usize {
+        if self.depth == maximum_depth {
+            return 1;
+        };
+
+        let steps_until_maximum_depth = maximum_depth - self.depth;
+
+        if let Some(number_of_children) = lookup_table
+            .get(&steps_until_maximum_depth)
+            .map(|lut| lut.get(&self.value))
+            .flatten() 
+        {
+            return *number_of_children;
+        };
+
+        transform_stone(self.value)
+            .iter()
+            .map(|ns_value| Self::new(self.depth, *ns_value))
+            .map(|sn| sn.count_children_recursively(maximum_depth, lookup_table))
+            .sum()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -99,8 +207,8 @@ mod tests {
         assert_eq!(run_first(true), 233875);
     }
 
-    // #[test]
-    // fn real_run_second() {
-    //     assert_eq!(run_second(true), xx);
-    // }
+    #[test]
+    fn real_run_second() {
+        assert_eq!(run_second(true), 277444936413293);
+    }
 }
